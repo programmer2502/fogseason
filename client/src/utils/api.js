b9b62855a349c9e614e76abb61095fd8d09d2fb0
aditya31031 <adityasaditya19@gmail.com>
2026-02-03 10:37:52 +0530

    fix admin issue

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://fogseason.vercel.app/api',
+    baseURL: 'https://www.fogseason.in/api',
 });
 
 // Add a request interceptor to include the auth token
