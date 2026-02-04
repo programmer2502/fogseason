ca28e7d14e3217cb1a7b5df697e46d21e9aa2c4c
aditya31031 <adityasaditya19@gmail.com>
2026-02-04 16:51:49 +0530

    test

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://fogseason.onrender.com/api',
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://fogseason.onrender.com/api',
+    // baseURL: 'http://localhost:5000/api',
 });
 
 // Add a request interceptor to include the auth token
