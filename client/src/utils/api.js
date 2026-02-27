53f750df53caacd188d781973c6cbcc240aba567
aditya31031 <adityasaditya19@gmail.com>
2026-02-27 15:17:29 +0530

    S

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'https://fogseason-production.up.railway.app',
-    baseURL: 'http://site--fogseason--m5cfswjrcr88.code.run',
+    baseURL: 'https://site--fogseason--m5cfswjrcr88.code.run',
     // baseURL: 'http://localhost:5000/api',
 });
 
